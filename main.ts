7c701c972caa29d73786ac90d015f87349ebe3dd
Brian Batchelder <brian@briansbrain.biz>
2023-06-19 09:42:06 -0700

    Find conflicts and display modal that allows user to select which files to clean. Todo: Clean.

--- main.ts
+++ main.ts
@@ -1,137 +1,128 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Plugin, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class CleanICloudSyncPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'CLean iCloud Sync Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// This adds a simple command that can be triggered from the palette
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'clean-icloud-sync-find-conflicts',
+			name: 'Find iCloud sync conflicts',
 			callback: () => {
-				new SampleModal(this.app).open();
+				this.findConflictsInMarkdownFiles();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+	}
+
+	// onunload() {
+	// }
+
+	// async loadSettings() {
+	// }
+
+	// async saveSettings() {
+	// }
+
+	async findConflictsInMarkdownFiles() {
+		let allFiles = this.app.vault.getMarkdownFiles().sort((a, b) => a.name.localeCompare(b.name))
+		if (allFiles.length == 0) { return };
+
+		const regex = /^(.*)( [0-9]+)(\.md)$/
+
+		var conflicts = new Map<string, Conflict>();
+		allFiles.forEach(function (file, index) { 
+			const matches = regex.exec(file.path)
+			if (matches) {
+				console.log("Might have found sync conflict: " + matches[0])
+				const originalFile = matches[1] + matches[3]
+				console.log("originalFile = " + originalFile)
+				if (allFiles.filter(e => e.path === originalFile).length > 0) {
+					if (file.path === originalFile) {
+						console.log(file.path + " most likely represents original file for a sync conflict.");
+					} else {
+						console.log(file.path + " most likely represents a sync conflict.");
+						var conflict = new Conflict([], false);
+						if (conflicts.has(originalFile)) {
+							conflict = conflicts.get(originalFile)!;
+						}
+						conflict.conflictingFiles.push(file.path);
+						conflicts.set(originalFile, conflict);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				} else {
+					console.log(file.path + " does not represent a sync conflict.");
 				}
+				console.log(" ")
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		conflicts.forEach((value, key) => {
+			console.log("value: ", value +
+				", key: ", key)
+		})
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		new SyncConflictModal(this.app, conflicts).open();
+	}	
+}
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+class Conflict {
+	conflictingFiles:Array<string>;
+	selected:boolean;
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+	constructor(conflictingFiles: Array<string>, selected: boolean) {
+		this.conflictingFiles = conflictingFiles;
+		this.selected = selected;
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class SyncConflictModal extends Modal {
+	conflicts: Map<string, Conflict>;
+
+	constructor(app: App, conflicts:Map<string, Conflict>) {
 		super(app);
+		this.conflicts = conflicts;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const {contentEl, titleEl} = this;
+
+		titleEl.setText('iCloud Sync Conflicts');
+
+		contentEl.createEl("p", { text: "Select which conflicts to clean." });
+
+		this.conflicts.forEach((value, key) => {
+			console.log("value: ", value +
+				", key: ", key);
+
+			new Setting(contentEl)
+			.setName(key)
+			.setDesc(value.conflictingFiles.join(", "))
+			.addToggle((toggle) =>
+					toggle
+					.onChange(async (shouldClean) => {
+						
+					})
+				);
+		})
+
+		new Setting(contentEl)
+		.addButton((btn) =>
+		  	btn
+				.setButtonText("Cancel")
+				.onClick(() => {
+					this.close();
+				}))
+		.addButton((btn) =>
+		  	btn
+				.setButtonText("Clean!")
+				.setCta()
+				.onClick(() => {
+					this.close();
+					//   clean!
+				}));
 	}
 
 	onClose() {
 		const {contentEl} = this;
 		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
